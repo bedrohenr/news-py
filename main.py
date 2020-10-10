1f76f9b2ce1ffb3cf8f4fd2b87ea145e5676ce16
ddrppx <ddrppx@gmail.com>
2020-10-10 11:31:13 -0300

    main.py init

---
+++ main.py
@@ -0,0 +1,9 @@
+print('## NEWS API - NEWSAPI.ORG ##\n')
+
+print("Insira as informações solicitadas abaixo")
+
+country = input("Insira o país da notícia (br, us): ")
+search = input("Insira sua pesquisa (palavra ou frase): ")
+source = input("Insira a fonte (globo, bbc, google-News): ")
+category = input("Insira a categoria (business, general): ")
+
