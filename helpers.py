52ff0c96dc8a3b7ea7ddf1b22e93fdec425f6f99
ddrppx <ddrppx@gmail.com>
2020-10-10 12:38:13 -0300

    added all_news() function

--- helpers.py
+++ helpers.py
@@ -1,13 +1,13 @@
-#Imports lib requests
+#imports lib requests
 import requests
-#Imports constants from config.py
+#imports constants from config.py
 from config import URL_BASE_TOP_HEADLINES, URL_BASE_EVERYTHING, API_KEY
 
 def top_news(country, source = None, category = None, search = None):
     """
-    Retorna as top notícias do dia
+    Returns the top news of newsapi.org
     """
-    #Variable checking (if exists)
+    #variable checking (if exists)
     if source:
         url = f"{URL_BASE_TOP_HEADLINES}sources={source}&apiKey={API_KEY}"
     elif category:
@@ -23,13 +23,41 @@ def top_news(country, source = None, category = None, search = None):
     #accessing the articles in the json (response)
     articles = response['articles']
 
-    #Empty array to store the articles's details
-    top_news_list = []
+    #empty array to store the articles's details
+    top_news = []
 
     #adding to the list via For loop (Title, Url, PublishedAt)
     for article in articles:
-        top_news_list.append(f"{article['title']}, " 
+        top_news.append(f"{article['title']}, " 
                              f"URL: {article['url']}, " 
                              f"Publicado em: {article['publishedAt']}")
 
-    print(top_news_list)
+    #returns the complete array
+    return top_news
+
+def all_news(search, language = None):
+    """
+    Returns all news from newsapi.org
+    """
+
+    #creates empty array
+    all_news = []
+
+    if search:
+        if language:
+            url = f"{URL_BASE_EVERYTHING}q={search}&language={language}&apiKey={API_KEY}"
+
+        #response in json using the lib requests
+        response = requests.get(url).json()
+
+        #accessing the articles in the json (response)
+        articles = response['articles']
+
+        #adding to the list via For loop (Title, Url, PublishedAt)
+        for article in articles:
+            all_news.append(f"{article['title']}, " 
+                                f"URL: {article['url']}, " 
+                                f"Publicado em: {article['publishedAt']}")
+
+    #returns the complete array
+    return all_news
